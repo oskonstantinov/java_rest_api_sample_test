10657e3a9d87645e823e4f832b85dc6b115e758f
Konstantinov Oleg <23136372+oskonstantinov@users.noreply.github.com>
2019-07-05 20:35:19 +0300

    Placed similar code into methods

--- rest-api-sample/src/test/java/ru/sample/rest/tests/RestTests.java
+++ rest-api-sample/src/test/java/ru/sample/rest/tests/RestTests.java
@@ -18,51 +18,70 @@ public class RestTests extends TestBase{
   @Test
   // The first test to check user "Samantha" presents in the base. If not, another tests with this user make no sense.
   public void searchSamantha() {
-    Response response = get(Endpoints.USERS)
-                       .then().statusCode(200)
-                       .and().extract().response();
-    List<String> users = response.jsonPath().getList("username");
+    List<String> users = getUsersByUsername();
     assertThat(users, hasItem(QueryParameters.MY_USER));
   }
 
   @Test
   // Used queries because if we don't have some specific user, we simply can change him to another
   public void searchSamanthaPosts() {
-    Response userResponse = given().queryParam(QueryParameters.USERNAME_PARAM, QueryParameters.MY_USER)
-                           .when().get(Endpoints.USERS)
-                           .then().statusCode(200)
-                           .and().extract().response();
-    String idFromJson = userResponse.jsonPath().getString("id");
-    Integer userId = Integer.parseInt(idFromJson.replaceAll("[^\\d]",""));
-    Response postsResponse = get(Endpoints.POSTS, userId)
-                            .then().statusCode(200)
-                            .and().extract().response();
-    List<Integer> samanthasPosts = postsResponse.jsonPath().getList("id");
-    assertThat(samanthasPosts.size(), equalTo(10));
+    List<Integer> samanthasPostsIds = getUserPostsIdsByQuery();
+    assertThat(samanthasPostsIds.size(), equalTo(10));
   }
 
   @Test
   public void validateEmailInComments() {
-    Response userResponse = given().queryParam(QueryParameters.USERNAME_PARAM, QueryParameters.MY_USER)
-                           .when().get(Endpoints.USERS)
-                           .then().statusCode(200)
-                           .and().extract().response();
-    String idFromJson = userResponse.jsonPath().getString("id");
-    Integer userId = Integer.parseInt(idFromJson.replaceAll("[^\\d]", ""));
-    Response postsResponse = get(Endpoints.POSTS, userId)
-                            .then().statusCode(200)
-                            .and().extract().response();
-    List<Integer> samanthasPosts = postsResponse.jsonPath().getList("id");
-    for (int i = 0; i < samanthasPosts.size(); i++) {
-      Integer postId = samanthasPosts.get(i);
-      Response commentsResponse = get(Endpoints.COMMENTS, postId)
-                                 .then().statusCode(200)
-                                 .and().extract().response();
-      List<String> emails = commentsResponse.jsonPath().getList("email");
+    List<Integer> samanthasPostsIds = getUserPostsIdsByQuery();
+    for (int i = 0; i < samanthasPostsIds.size(); i++) {
+      Integer postId = samanthasPostsIds.get(i);
+      List<String> emails = getUsersEmailsFromComments(postId);
       for (int e = 0; e < emails.size(); e++) {
         String email = emails.get(e);
         assertTrue(EmailValidator.getInstance().isValid(email));
       }
     }
+  }
+
+  private List<String> getUsersByUsername() {
+    Response my_response = getResponse(Endpoints.USERS);
+    List<String> users = my_response.jsonPath().getList("username");
+    return users;
+  }
+
+  private List<Integer> getUserPostsIdsByQuery() {
+    Response userResponse = getResponseWithQuery(Endpoints.USERS, QueryParameters.USERNAME_PARAM, QueryParameters.MY_USER);
+    String idFromJson = userResponse.jsonPath().getString("id");
+    Integer userId = Integer.parseInt(idFromJson.replaceAll("[^\\d]",""));
+    Response postsResponse = getResponseWithParameter(Endpoints.POSTS, userId);
+    List<Integer> userPostsIds = postsResponse.jsonPath().getList("id");
+    return userPostsIds;
+  }
+
+  private List<String> getUsersEmailsFromComments(int postId) {
+    Response commentsResponse = getResponseWithParameter(Endpoints.COMMENTS, postId);
+    List<String> emails = commentsResponse.jsonPath().getList("email");
+    return emails;
+  }
+
+  private Response getResponseWithParameter(String endpoint, Integer id) {
+    Response response = get(endpoint, id)
+            .then().statusCode(200)
+            .and().extract().response();
+    return response;
+  }
+
+  private Response getResponse(String endpoint) {
+    Response response = get(endpoint)
+            .then().statusCode(200)
+            .and().extract().response();
+    return response;
+  }
+
+  private Response getResponseWithQuery(String endpoint, String queryParam, String value) {
+    Response response = given().queryParam(queryParam, value)
+            .when().get(endpoint)
+            .then().statusCode(200)
+            .and().extract().response();
+    return response;
   }
 }
