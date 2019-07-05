52adbaa36740ebc629f9f0f0f4d452834415c8f2
Konstantinov Oleg <23136372+oskonstantinov@users.noreply.github.com>
2019-07-05 19:35:00 +0300

    Added third test and Apache Commons

--- rest-api-sample/src/test/java/ru/sample/rest/helpers/Endpoints.java
+++ rest-api-sample/src/test/java/ru/sample/rest/helpers/Endpoints.java
@@ -3,4 +3,5 @@ package ru.sample.rest.helpers;
 public final class Endpoints {
   public static final String USERS = "/users";
   public static final String POSTS = "/posts?userId={userId}";
+  public static final String COMMENTS = "/comments?postId={postId}";
 }
--- rest-api-sample/src/test/java/ru/sample/rest/tests/RestTests.java
+++ rest-api-sample/src/test/java/ru/sample/rest/tests/RestTests.java
@@ -1,6 +1,7 @@
 package ru.sample.rest.tests;
 
 import com.jayway.restassured.response.Response;
+import org.apache.commons.validator.routines.EmailValidator;
 import org.testng.annotations.Test;
 import ru.sample.rest.helpers.Endpoints;
 import ru.sample.rest.helpers.QueryParameters;
@@ -10,14 +11,16 @@ import java.util.List;
 import static com.jayway.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
+import static org.testng.Assert.assertTrue;
 
 public class RestTests extends TestBase{
 
   @Test
   // The first test to check user "Samantha" presents in the base. If not, another tests with this user make no sense.
   public void searchSamantha() {
-    Response response = get(Endpoints.USERS).then().statusCode(200)
-            .and().extract().response();
+    Response response = get(Endpoints.USERS)
+                       .then().statusCode(200)
+                       .and().extract().response();
     List<String> users = response.jsonPath().getList("username");
     assertThat(users, hasItem(QueryParameters.MY_USER));
   }
@@ -26,15 +29,40 @@ public class RestTests extends TestBase{
   // Used queries because if we don't have some specific user, we simply can change him to another
   public void searchSamanthaPosts() {
     Response userResponse = given().queryParam(QueryParameters.USERNAME_PARAM, QueryParameters.MY_USER)
-            .when().get(Endpoints.USERS)
-            .then().statusCode(200)
-            .and().extract().response();
+                           .when().get(Endpoints.USERS)
+                           .then().statusCode(200)
+                           .and().extract().response();
     String idFromJson = userResponse.jsonPath().getString("id");
     Integer userId = Integer.parseInt(idFromJson.replaceAll("[^\\d]",""));
     Response postsResponse = get(Endpoints.POSTS, userId)
-            .then().statusCode(200)
-            .and().extract().response();
+                            .then().statusCode(200)
+                            .and().extract().response();
     List<Integer> samanthasPosts = postsResponse.jsonPath().getList("id");
     assertThat(samanthasPosts.size(), equalTo(10));
+  }
+
+  @Test
+  public void validateEmailInComments() {
+    Response userResponse = given().queryParam(QueryParameters.USERNAME_PARAM, QueryParameters.MY_USER)
+                           .when().get(Endpoints.USERS)
+                           .then().statusCode(200)
+                           .and().extract().response();
+    String idFromJson = userResponse.jsonPath().getString("id");
+    Integer userId = Integer.parseInt(idFromJson.replaceAll("[^\\d]", ""));
+    Response postsResponse = get(Endpoints.POSTS, userId)
+                            .then().statusCode(200)
+                            .and().extract().response();
+    List<Integer> samanthasPosts = postsResponse.jsonPath().getList("id");
+    for (int i = 0; i < samanthasPosts.size(); i++) {
+      Integer postId = samanthasPosts.get(i);
+      Response commentsResponse = get(Endpoints.COMMENTS, postId)
+                                 .then().statusCode(200)
+                                 .and().extract().response();
+      List<String> emails = commentsResponse.jsonPath().getList("email");
+      for (int e = 0; e < emails.size(); e++) {
+        String email = emails.get(e);
+        assertTrue(EmailValidator.getInstance().isValid(email));
+      }
+    }
   }
 }
